e00d97ad3f27ce537525321bb3ab14060931d6f0
Ibragf <gaziibragim@mail.ru>
2022-05-12 10:54:03 +0300

    first stage

---
+++ FileManager/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileManager
+{
+    public partial class MainWindow : Window
+    {
+        private int SelectedIndex = -1;
+        public List<Info> components { get; set; }
+        private Stack<string> LastElements;
+        public MainWindow()
+        {
+            components = new List<Info>();
+            LastElements = new Stack<string>();
+            InitializeComponent();
+            ShowDirs.openDrives(components);
+            DataContext = this;
+        }
+
+        private void ListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (sender.GetType() == typeof(ListView))
+            {
+                try
+                {
+                    ListView view = (ListView)sender;
+                    int index = view.SelectedIndex;
+                    ShowDirs.openFileOrDir(index, components, LastElements);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    
+                }
+                finally
+                {
+                    phonesList.Items.Refresh();
+                }
+            }
+        }
+
+        public void delete(string path)
+        {
+            if(!Directory.Exists(path))
+            {
+                return;
+            }
+            if (Directory.Exists(path) && Directory.GetFileSystemEntries(path).Length==0)
+            {
+                Directory.Delete(path);
+                return;
+            }
+            DirectoryInfo directory = new DirectoryInfo(path);
+            string Pat = directory.ToString();
+            if(directory.Exists)
+            {
+                FileInfo[] files = directory.GetFiles();
+                if (files.Length>0)
+                {
+                    foreach (FileInfo file in files)
+                    {
+                        file.Delete();
+                    }
+                }
+
+                DirectoryInfo[] dirs = directory.GetDirectories();
+                if(dirs.Length>0)
+                {
+                    foreach (var dir in dirs)
+                    {
+                        delete(dir.ToString());
+                    }
+                }
+            }
+            delete(path);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(LastElements.Count>0)
+            {
+                ShowDirs.openFileOrDir(LastElements.Pop(), components);
+                phonesList.Items.Refresh();
+                if (LastElements.Count > 0) textBox.Text = Directory.GetParent((components[0].Element.ToString())).ToString();
+            }
+        }
+
+        private void phonesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(LastElements.Count>0 && components.Count>0) textBox.Text = Directory.GetParent((components[0].Element.ToString())).ToString();
+        }
+    }
+
+    public class Info
+    {
+        public Info(string name, string date, object Element)
+        {
+            this.name = name;
+            lastWriteDate = date;
+            this.Element = Element;
+        }
+
+        public Info(string name, string date,string extension, object Element)
+        {
+            this.name = name;
+            this.extension = extension;
+            this.Element = Element;
+            lastWriteDate = date;
+        }
+        public string name { get; set; } = "";
+        public string lastWriteDate { get; set; }="";
+        public object Element { get; set; } = null;
+
+        public string extension { get; set; } = "";
+    }
+}
---
+++ FileManager/ShowDirs.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+    internal static class ShowDirs
+    {
+        public static void openDrives(List<Info> components)
+        {
+            components.Clear();
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                Info info = new Info(drive.Name, drive.DriveType.ToString(), drive);
+                components.Add(info);
+            }
+        }
+
+        public static void openFileOrDir(int index, List<Info> components, Stack<string> LastElements)
+        {
+            if (index == -1) return;
+            if (components[index].Element.GetType() == typeof(DriveInfo))
+            {
+                LastElements.Push("drive");
+                string path = components[index].Element.ToString();
+                components.Clear();
+                string[] dirs = Directory.GetDirectories(path);
+                foreach (string dir in dirs)
+                {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(dir);
+                    Info info = new Info(directoryInfo.Name, directoryInfo.LastWriteTime.ToShortDateString(), directoryInfo);
+                    components.Add(info);
+                }
+                foreach (string file in Directory.GetFiles(path))
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    Info info = new Info(fileInfo.Name, fileInfo.LastWriteTime.ToShortDateString(),fileInfo.Extension, fileInfo);
+                    components.Add(info);
+                }
+                return;
+            }
+            if (components[index].Element.GetType() == typeof(DirectoryInfo))
+            {
+                LastElements.Push(Directory.GetParent(components[index].Element.ToString()).ToString());
+                string path = components[index].Element.ToString();
+                components.Clear();
+                string[] dirs = Directory.GetDirectories(path);
+                foreach (string dir in dirs)
+                {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(dir);
+                    Info info = new Info(directoryInfo.Name, directoryInfo.LastWriteTime.ToShortDateString(), directoryInfo);
+                    components.Add(info);
+                }
+                foreach (string file in Directory.GetFiles(path))
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    Info info = new Info(fileInfo.Name, fileInfo.LastWriteTime.ToShortDateString(), fileInfo.Extension , fileInfo);
+                    components.Add(info);
+                }
+                return;
+            }
+            if (components[index].Element.GetType() == typeof(FileInfo))
+            {
+                ProcessStartInfo process = new ProcessStartInfo();
+                process.UseShellExecute = true;
+                process.FileName = components[index].Element.ToString();
+                Process.Start(process);
+            }
+        }
+
+        public static void openFileOrDir(string lastElement, List<Info> components)
+        {
+            if (lastElement == "drive")
+            {
+                components.Clear();
+                openDrives(components);
+                return;
+            }
+            string path = lastElement;
+            components.Clear();
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (string dir in dirs)
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(dir);
+                Info info = new Info(directoryInfo.Name, directoryInfo.LastWriteTime.ToShortDateString(), directoryInfo);
+                components.Add(info);
+            }
+            foreach (string file in Directory.GetFiles(path))
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                Info info = new Info(fileInfo.Name, fileInfo.LastWriteTime.ToShortDateString(), fileInfo);
+                components.Add(info);
+            }
+        }
+    }
+}
